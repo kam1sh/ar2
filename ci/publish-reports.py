1d0a297141aeccda543b177d9707ea35fea1a1de
Igor Ovsyannikov <kamish@outlook.com>
2020-06-10 20:04:16 +0300

    Improved report script

--- ci/publish-reports.py
+++ ci/publish-reports.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
-from subprocess import run
+import subprocess
 from os import getenv
+
+def run(*args):
+    try:
+        subprocess.run(list(args), check=True)
+    except subprocess.CalledProcessError as e:
+        with open("error.log", "w") as fd:
+            print(f"Error executing {args}", file=fd)
+            print(e, file=fd)
 
 identity    = getenv("REPORTS_SSH_KEYFILE")
 host        = getenv("REPORTS_HOST")
@@ -10,6 +18,6 @@ slug        = getenv("JOB_NAME")
 slug = slug.replace("/", "-")
 # sync report to reports container
 print("Synchronizing report with server...")
-run(["rsync", "-av", "-e", f"ssh -i {identity}", "build/reports/allure-report/", f"{host}:{slug}/"])
-run(["ssh", "-i", identity, "ln", "-s", slug, "latest"])
-print(f"Report available at http://{host}/{slug}")
+run(["rsync", "-av", "-e", f"ssh -i {identity}", "build/reports/allure-report/", f"{user}@{host}:{slug}/"])
+run(["ssh", "-i", identity, f"{user}@{host}", "ln", "-s", slug, "latest"])
+print(f"Report available at {slug}")
